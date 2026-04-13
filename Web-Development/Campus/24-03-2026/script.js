7831f1ca3190fe70a7c734ead69588231441c39c
Juan Duarte Moura <juanduarte050508@gmail.com>
2026-04-13 13:47:45 -0300

    Add files via upload

---
+++ Web-Development/Campus/24-03-2026/script.js
@@ -0,0 +1,75 @@
+// let idade = 25
+// let temCarteira = true 
+
+// if(idade >=18 && temCarteira) 
+//     {console.log("Pode Dirigir")}
+
+// if (idade < 18 || !temCarteira) 
+//     {console.log ("Não pode dirigir")}
+
+// if (!temCarteira)
+//     {console.log ("Não pode dirigir")}
+
+// let tarefaConcluida = true
+
+// if (tarefaConcluida) 
+//     {console.log("A tarefa está concluida")}
+
+// let prioridade = 3
+
+// // if (prioridade === 3) 
+// //     {console.log("priroidade alta")}
+// // else {console.log("priroidade baixa")}
+
+// // let prioridade2 = (prioridade ===3 ? 
+// // "prioridade Alta" : "prioridade baixa")
+
+// if (prioridade === 1)
+//     {console.log("prioridade Baixa")}
+
+// else if (prioridade ===2)
+// {console.log("Prioridade Média")}
+
+// else if (prioridade ===3)
+// {console.log("Prioridade Alta")}
+
+// else 
+// {console.log("Prioridade desconhecida")}
+
+// let diaSemana = new Date ().getDay
+// ()
+
+// console.log(diaSemana)
+
+// if(diaSemana === 0) 
+//     {console.log("Domingo")}
+// else if (diaSemana === 1)
+//      {console.log("Segunda")}
+// else if (diaSemana === 2)
+//      {console.log("Terça")}
+// else if (diaSemana === 3)
+//      {console.log("Quarta")}
+// else if (diaSemana === 4)
+//      {console.log("Quinta")}
+// else if (diaSemana === 5)
+//     {console.log("Sexta")}
+// else if (diaSemana === 6)
+//     {console.log("sabado")}
+
+// 
+// for (let i = 0;i <= 5;i++) 
+// {console.log("contagem:", i)
+
+// let contador = 0
+
+// while (contador >= -5) 
+// {console.log("Contagem:" + contador)
+// contador--}
+
+let numero = 5 
+
+do {console.log("Numero é:", numero)
+    numero--}
+    while (numero > 0)
+
+
